541423f91d5034132733920b9d39ac13d4175755
kolys <kolys.dev@gmail.com>
2016-11-16 19:49:28 +0300

    d

---
+++ CreativeSite/Controllers/ChaptersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CreativeSite.Models;
+
+namespace CreativeSite.Controllers
+{
+    public class ChaptersController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Chapters
+        public ActionResult Index()
+        {
+            return View(db.Chapters.ToList());
+        }
+
+        // GET: Chapters/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Chapter chapter = db.Chapters.Find(id);
+            if (chapter == null)
+            {
+                return HttpNotFound();
+            }
+            return View(chapter);
+        }
+
+        // GET: Chapters/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Chapters/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,Content")] Chapter chapter)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Chapters.Add(chapter);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(chapter);
+        }
+
+        // GET: Chapters/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Chapter chapter = db.Chapters.Find(id);
+            if (chapter == null)
+            {
+                return HttpNotFound();
+            }
+            return View(chapter);
+        }
+
+        // POST: Chapters/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,Content")] Chapter chapter)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(chapter).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(chapter);
+        }
+
+        // GET: Chapters/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Chapter chapter = db.Chapters.Find(id);
+            if (chapter == null)
+            {
+                return HttpNotFound();
+            }
+            return View(chapter);
+        }
+
+        // POST: Chapters/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Chapter chapter = db.Chapters.Find(id);
+            db.Chapters.Remove(chapter);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CreativeSite/Controllers/CreativesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CreativeSite.Models;
+
+namespace CreativeSite.Controllers
+{
+    public class CreativesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Creatives
+        public ActionResult Index()
+        {
+            return View(db.Creatives.ToList());
+        }
+
+        // GET: Creatives/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Creative creative = db.Creatives.Find(id);
+            if (creative == null)
+            {
+                return HttpNotFound();
+            }
+            return View(creative);
+        }
+
+        // GET: Creatives/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Creatives/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title")] Creative creative)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Creatives.Add(creative);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(creative);
+        }
+
+        // GET: Creatives/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Creative creative = db.Creatives.Find(id);
+            if (creative == null)
+            {
+                return HttpNotFound();
+            }
+            return View(creative);
+        }
+
+        // POST: Creatives/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title")] Creative creative)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(creative).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(creative);
+        }
+
+        // GET: Creatives/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Creative creative = db.Creatives.Find(id);
+            if (creative == null)
+            {
+                return HttpNotFound();
+            }
+            return View(creative);
+        }
+
+        // POST: Creatives/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Creative creative = db.Creatives.Find(id);
+            db.Creatives.Remove(creative);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
