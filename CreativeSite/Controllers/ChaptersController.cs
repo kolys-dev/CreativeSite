6f5255fb2273ada82426397328bd03b2cabca745
kolys <kolys.dev@gmail.com>
2016-11-23 22:05:53 +0300

    thyresdfgyw4

--- CreativeSite/Controllers/ChaptersController.cs
+++ CreativeSite/Controllers/ChaptersController.cs
@@ -76,6 +76,7 @@ namespace CreativeSite.Controllers
         // POST: Chapters/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [ValidateInput(false)]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,Title,Content")] Chapter chapter)
